7ccba5817567958a36adc8c23f268fbb69771cc2
fatroo <rm552184@fiap.com.br>
2024-05-16 09:42:52 -0300

    Add project files.

---
+++ Models/TendenciasGastos.cs
@@ -0,0 +1,10 @@
+﻿namespace Sprint2.Models
+{
+    public class TendenciasGastos
+    {
+        public long Id { get; set; }
+        public long Ano { get; set; }
+        public double GastoMarketing { get; set; }
+        public double GastoAutomacao { get; set; }
+    }
+}
