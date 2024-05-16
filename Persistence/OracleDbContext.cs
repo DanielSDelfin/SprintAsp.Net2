e8c6804cb8bb0502b5321acf111a1f26d0d295ff
fatroo <rm552184@fiap.com.br>
2024-05-16 11:36:27 -0300

    att

--- Persistence/OracleDbContext.cs
+++ Persistence/OracleDbContext.cs
@@ -5,15 +5,13 @@ namespace Sprint2.Persistence
 {
     public class OracleDbContext : DbContext
     {
-        public OracleDbContext(DbContextOptions<OracleDbContext> options) : base(options)
-        {
-
-        }
-
         public DbSet<ComportamentoNegocios> ComportamentoNegocios { get; set; }
         public DbSet<TendenciasGastos> TendenciasGastos { get; set; }
         public DbSet<DesempenhoFinanceiro> DesempenhoFinanceiro { get; set; }
+        public OracleDbContext(DbContextOptions<OracleDbContext> options) : base(options)
+        {
+        }
+
     }
-    {
-    }
+    
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Sprint2.Persistence;
+
 namespace Sprint2
 {
     public class Program
@@ -5,7 +8,12 @@ namespace Sprint2
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            
+            // Add DbContext Oracle on the project
+            builder.Services.AddDbContext<OracleDbContext>(options =>
+            {
+                options.UseOracle(builder.Configuration.GetConnectionString("OracleFiap"));
+            });
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
